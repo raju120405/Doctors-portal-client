e2da7096e59f91cffb63110f3412ff028304f524
raju120405 <raju120405@gmail.com>
2022-05-11 15:48:11 +0600

    Exceptional Dental Care

--- src/Pages/Home/Info.js
+++ src/Pages/Home/Info.js
@@ -5,9 +5,11 @@ import marker from '../../assets/icons/marker.svg'
 import phone from '../../assets/icons/phone.svg'
 const Info = () => {
     return (
-        <div className='grid grid-cols-1 lg:grid-cols-3 gap-5 px-12'>
+        <div className='grid grid-cols-1 lg:grid-cols-3 gap-5 '>
             <InfoCart cardTitle="opening Hours" bgClass="bg-gradient-to-r from-secondary to-primary" img={clock}></InfoCart>
+
             <InfoCart cardTitle="Our Location" bgClass="bg-accent" img={marker}></InfoCart>
+
             <InfoCart cardTitle="Contact Us" bgClass="bg-gradient-to-r from-secondary to-primary" img={phone}></InfoCart>
         </div>
     );
--- src/Pages/Shared/navbar/Navbar.js
+++ src/Pages/Shared/navbar/Navbar.js
@@ -11,7 +11,7 @@ const Navbar = () => {
         <li><Link to= "/login">Login</Link></li>
     </>
     return (
-        <div class="navbar bg-base-100">
+        <div class="navbar bg-base-100 px-12">
             <div class="navbar-start">
                 <div class="dropdown">
                     <label tabindex="0" class="btn btn-ghost lg:hidden">
@@ -23,7 +23,7 @@ const Navbar = () => {
                 </div>
                 <a class="btn btn-ghost normal-case text-xl">Doctors Portal</a>
             </div>
-            <div class="navbar-center hidden lg:flex">
+            <div class="navbar-center hidden lg:flex px-12">
                 <ul class="menu menu-horizontal p-0">
                   {meanuItems}
                 </ul>
