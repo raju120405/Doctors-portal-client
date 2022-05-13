598bff0cbbed5e7d75b15bb69ac2c2b88a3de923
raju120405 <raju120405@gmail.com>
2022-05-13 22:58:05 +0600

    servicr cart add

--- src/Pages/Shared/navbar/Appointment/AvailableAppoinment.js
+++ src/Pages/Shared/navbar/Appointment/AvailableAppoinment.js
@@ -1,11 +1,25 @@
 import { format } from 'date-fns';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import Service from './Service';
 
 const AvailableAppoinment = ({date}) => {
-    
+  const [services,setServices]=useState([]);
+  useEffect(()=>{
+      fetch('services.json')
+      .then(res=>res.json())
+      .then(data =>setServices(data));
+  },[])
     return (
         <div>
-            Available Appointments on {format(date,'PP')}
+            <h2 className='text-center text-secondary text-xl'>Available Appointments on {format(date,'PP')}</h2>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+            {
+                services.map(service=><Service
+                key={service._id}
+                service={service}
+                ></Service>)
+            }
+            </div>
         </div>
     );
 };
