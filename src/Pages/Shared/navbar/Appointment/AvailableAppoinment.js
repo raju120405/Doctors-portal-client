51032758767cfe61e7d6be2d7060f3dd85950a73
raju120405 <raju120405@gmail.com>
2022-05-13 23:56:37 +0600

    modal working

--- src/Pages/Shared/navbar/Appointment/AvailableAppoinment.js
+++ src/Pages/Shared/navbar/Appointment/AvailableAppoinment.js
@@ -1,25 +1,30 @@
 import { format } from 'date-fns';
 import React, { useEffect, useState } from 'react';
+import BookingModal from './BookingModal';
 import Service from './Service';
 
-const AvailableAppoinment = ({date}) => {
-  const [services,setServices]=useState([]);
-  useEffect(()=>{
-      fetch('services.json')
-      .then(res=>res.json())
-      .then(data =>setServices(data));
-  },[])
+const AvailableAppoinment = ({ date }) => {
+    const [services, setServices] = useState([]);
+    const [treatment,setTreatment] = useState(null);
+
+    useEffect(() => {
+        fetch('services.json')
+            .then(res => res.json())
+            .then(data => setServices(data));
+    }, [])
     return (
         <div>
-            <h2 className='text-center text-secondary text-xl'>Available Appointments on {format(date,'PP')}</h2>
+            <h2 className='text-center text-secondary text-xl'>Available Appointments on {format(date, 'PP')}</h2>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
-            {
-                services.map(service=><Service
-                key={service._id}
-                service={service}
-                ></Service>)
-            }
+                {
+                    services.map(service => <Service
+                        key={service._id}
+                        service={service}
+                        setTreatment={setTreatment}
+                    ></Service>)
+                }
             </div>
+            {treatment && <BookingModal treatment={treatment}></BookingModal>}
         </div>
     );
 };
--- src/Pages/Shared/navbar/Appointment/Service.js
+++ src/Pages/Shared/navbar/Appointment/Service.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Service = ({ service }) => {
+const Service = ({ service, setTreatment }) => {
     const { name, slots } = service;
     return (
 
@@ -16,7 +16,10 @@ const Service = ({ service }) => {
                 </p>
                 <p>{slots.length} {slots.length > 1 ? 'Spaces': 'Space'} Available</p>
                 <div class="card-actions justify-center">
-                    <button disabled={slots.length===0} class="btn btn-secondary text-white uppercase">Book Appointment</button>
+                    
+                     <label for="booking-modal"
+                     disabled={slots.length===0}
+                     onClick={()=>setTreatment(service)} class="btn btn-secondary text-white uppercase">Book Appointment</label>
                 </div>
             </div>
         </div>
